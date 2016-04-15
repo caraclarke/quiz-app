c1b3863b7d437151036082e559b2467cc229a6fd
caraclarke <cara.clarke8@gmail.com>
2016-04-15 09:40:36 -0400

    Components added, webpack added, page rendering test

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <p>App</p>
+      </div>
+    );
+  }
+}
+
+module.exports = App;
